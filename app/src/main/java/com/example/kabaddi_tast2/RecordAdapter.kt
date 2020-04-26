e1a113cbc95d8ae2ef0d3e97fdc407dcfdfab826
Hsuan-Yi,Lin <lovevul35j6@gmail.com>
2020-04-26 09:17:55 +0800

    Activvity項目： RecordAdapter.kt RecordLiestActivity.kt layout增加項目： row_record.xml record_liest.xml

---
+++ app/src/main/java/com/example/kabaddi_tast2/RecordAdapter.kt
@@ -0,0 +1,2 @@
+package com.example.kabaddi_tast2
+
---
+++ app/src/main/java/com/example/kabaddi_tast2/RecordLiestActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kabaddi_tast2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecordLiestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_record_liest)
+    }
+}
