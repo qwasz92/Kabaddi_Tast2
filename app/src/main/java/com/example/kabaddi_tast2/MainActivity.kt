3bcdc972af6b7a3da50fa955460572be7b5c514c
Hsuan-Yi,Lin <lovevul35j6@gmail.com>
2020-04-28 14:57:23 +0800

    MainActivity 增加活動: 在MainActivity定義functions(清單元件裡面內容為「新增&查詢」) 取得recycler的布局元件在LinearLayoutManager，再將物件尺寸固定，最後將元件顯示在FunctionAdapter 建立inner class FunctionAdapter 是繼承RecyclerView的適配器(Adapter)<顯示FunctionHolder的物件> 建立FunctionHolder是一個顯示內容物的位置，顯示在row_function裡面的row_function_tv_name 在FunctionHolder裡面建立：資料內容、資料的取得、顯示資料的位置 layout main 修正: 增加id layout row_function修正: 增加id與修正id

--- app/src/main/java/com/example/kabaddi_tast2/MainActivity.kt
+++ app/src/main/java/com/example/kabaddi_tast2/MainActivity.kt
@@ -2,11 +2,44 @@ package com.example.kabaddi_tast2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.row_function.view.*
 
 class MainActivity : AppCompatActivity() {
+    //        RecyclerView(清單元件)
+    val functions = listOf<String>("新增", "查詢")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        recycler.layoutManager = LinearLayoutManager(this)
+        recycler.setHasFixedSize(true)
+        recycler.adapter = FunctionAdapter()
+    }
+
+    inner class FunctionAdapter : RecyclerView.Adapter<FunctionHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FunctionHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.row_function, parent, false)
+            val holder = FunctionHolder(view)
+            return holder
+        }
+
+        override fun getItemCount(): Int {
+            return functions.size
+        }
+
+        override fun onBindViewHolder(holder: FunctionHolder, position: Int) {
+            holder.nameText.text = functions.get(position)
+        }
+    }
+
+    class FunctionHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var nameText: TextView = view.row_function_tv_name
     }
 }
